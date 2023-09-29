719cb7f19906b8cc987e206cc8c069d810000a80
NattyNgrn <natnrgn@gmail.com>
2023-09-29 15:09:51 -0400

    ughhh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Search from './components/search';
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [weather, setWeather] = useState({});
 
   return (
 
       <div>
         <img class="h-auto max-w-full" src="https://www.seathecity.com/wp-content/uploads/2021/04/new-york-3602856_640.jpg" alt="image description"></img>
+        <Search setWeather={setWeather} > </Search>
       </div>
   )
 }
--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -1,15 +1,20 @@
+import getWeather from "../serverfuncs"
+import { useState, useEffect } from "react"
+const [weather, setWeather] = useState({
 
-
-function Search(){
+})
+function Search({setWeather}){
 
     return(
         <div>
             <form>
-                <input type="text" placeholder="Enter a city" class="border border-gray-500 rounded-lg py-3 px-6 w-64"/>
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md">
+                <input onChange={(e)=> {}} type="text" placeholder="Enter a city" class="border border-gray-500 rounded-lg py-3 px-6 w-64"/>
+                <button onClick={getWeatherData} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md">
                     Search
                 </button>
             </form>
         </div>
     )
 }
+
+export default Search;
